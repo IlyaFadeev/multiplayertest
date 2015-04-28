73754a4c31a5efeaaed1640758e1a6b75ff8e399
IlyaFadeev <slide_93@mail.ru>
2015-04-28 18:45:41 +0400

    Initial commit

---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,68 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+
+/**
+ * Created by Ilya on 28.04.2015.
+ */
+public class MyGdxGame extends Game {
+
+    private static MyGdxGame instance = new MyGdxGame();
+    Warrior warrior;
+    Stage stage;
+    class KeysListener extends InputListener {
+        @Override
+        public boolean keyDown (InputEvent event, int keycode) {
+            float x = event.getListenerActor().getX();
+            float y = event.getListenerActor().getY();
+
+            switch(keycode) {
+                case Input.Keys.LEFT:
+                    x -= 1;
+                    break;
+                case Input.Keys.RIGHT:
+                    x += 1;
+                    break;
+                case Input.Keys.UP:
+                    y += 1;
+                    break;
+                case Input.Keys.DOWN:
+                    y -= 1;
+                    break;
+            }
+            event.getListenerActor().setPosition(x, y);
+            return false;
+        }
+    }
+    public void create()
+    {
+        stage = new Stage();
+        Gdx.input.setInputProcessor(stage);
+        warrior = new Warrior(new Texture("C:\\Users\\Ilya\\Documents\\multiplayertest\\warrior.png"));
+        warrior.addListener(new KeysListener());
+        stage.addActor(warrior);
+        stage.setKeyboardFocus(warrior);
+    }
+
+
+
+    public static MyGdxGame getInstance() {
+        return instance;
+    }
+
+    private MyGdxGame(){}
+    public void render()
+    {
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        stage.draw();
+        stage.act(Gdx.graphics.getDeltaTime());
+
+    }
+}
---
+++ core/src/com/mygdx/game/Warrior.java
@@ -0,0 +1,31 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+
+import java.awt.event.InputEvent;
+
+/**
+ * Created by Ilya on 28.04.2015.
+ */
+public class Warrior extends Actor{
+    private Texture toDraw;
+
+
+
+    public Warrior(Texture toDraw)
+    {
+        this.toDraw = toDraw;
+        setSize(50, 50);
+        setPosition(10, 10);
+    }
+
+    public void draw(Batch batch, float parentAlpha)
+    {
+        batch.setColor(getColor());
+        batch.draw(toDraw, getX(), getY(), getWidth(), getHeight());
+    }
+}
